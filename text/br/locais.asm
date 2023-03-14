a9ecd7defd7360c9fc6bea4916f140a654d4dea9
ox-carnage <ox_carnage@msn.com>
2023-03-14 09:45:55 -0300

    Atualizado

--- text/br/locais.asm
+++ text/br/locais.asm
@@ -1,13 +1,13 @@
 origin $000841E1
 location_00:
-text_location("FASE CAVERNA COMPLETADA!!!")
+    text_location("FASE CAVERNA COMPLETADA!!!")
 location_01:
-text_location("FASE CIDADE COMPLETADA!!!")
+    text_location("FASE CIDADE COMPLETADA!!!")
 location_02:
-text_location("FASE CENTRAL PARK COMPLETADA!!!")
+    text_location("FASE CENTRAL PARK COMPLETADA!!!")
 location_03:
-text_location("FASE ESTACAO ELETRICA COMPLETDA!!!")
+    text_location("FASE ESTACAO ELETRICA COMPLETDA!!!")
 location_04:
-text_location("FASE DO ESCOTO COMPLETADA!!!")
+    text_location("FASE DO ESCOTO COMPLETADA!!!")
 location_05:
-text_location("FASE ARMAZEM DO CAIS COMPLETADA!!!")
+    text_location("FASE ARMAZEM DO CAIS COMPLETADA!!!")
